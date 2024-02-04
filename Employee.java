08f522745dcf4747a0c333e3b9b9595051975ffc
tlc67 <kris7cuevas@gmail.com>
2024-02-04 14:38:26 -0500

    built out employee doctor classes

--- Employee.java
+++ Employee.java
@@ -1,8 +1,14 @@
 public abstract class Employee {
     int patientHealth;
+    double salary;
+    String employeeName;
+    int employeeNumber;
 
-    public Employee() {
+    public Employee(double salary, String employeeName, int employeeNumber) {
         this.patientHealth = 10;
+        this.salary = salary;
+        this.employeeName = employeeName;
+        this.employeeNumber = employeeNumber;
     }
 
     public int getPatientHealth() {
@@ -11,6 +17,31 @@ public abstract class Employee {
 
     public void setPatientHealth(int patientHealth) {
         this.patientHealth = patientHealth;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getEmployeeName() {
+        return this.employeeName;
+
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getEmployeeNumber() {
+        return this.employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
     }
 
     @Override
